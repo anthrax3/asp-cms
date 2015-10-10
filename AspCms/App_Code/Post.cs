a3471f90116e2af2ee695704b90fd3a0704f11f9
Milos Kovacevic <miloscpt@gmail.com>
2015-10-10 20:15:06 +0200

    added form for posting + Post class

---
+++ AspCms/App_Code/Post.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Web;
+using System.Web.WebPages;
+
+/// <summary>
+/// Summary description for Post
+/// </summary>
+public class Post
+{
+	public Post()
+	{
+		
+	}
+
+    private static WebPageRenderingBase Page 
+    {
+        get 
+        {
+            return WebPageContext.Current.Page;
+        }
+    }
+
+    public static string Mode
+    {
+        get 
+        {
+            if (Page.UrlData.Any())
+            {
+                return Page.UrlData[0].ToLower();
+            }
+            return string.Empty;
+        }
+    }
+
+    public static string Slug
+    {
+        get
+        {
+            if (Mode == "edit")
+            {
+                return Page.UrlData[1];
+            }
+
+            return string.Empty;
+        }
+    }
+
+    public static dynamic Current
+    {
+        get 
+        { 
+            
+        }
+    }
+
+    private static dynamic CreatePostObject()
+    {
+        dynamic obj = new ExpandoObject();
+
+        obj.Title = "";
+        obj.Content = "";
+        obj.DateCreated = DateTime.Now;
+        obj.DataPublished = null;
+    }
+
+
+
+
+
+
+
+}
